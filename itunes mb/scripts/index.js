ef8e0e0c2b4c45936aeee71ed08b6d333cb592f7
FerryFrozen <63611645+FerryFrozen@users.noreply.github.com>
2020-07-27 22:48:24 +0300

    [NF] Initial commit

---
+++ itunes mb/scripts/index.js
@@ -0,0 +1,23 @@
+import {radioPlayerInit} from './radioPlayer.js';
+import {videoPlayerInit} from './videoPlayer.js';
+import {musicPlayerInit} from './musicPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deactivatePrevious = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(btn => btn.classList.remove('active'));
+    playerBlock.forEach(block => block.classList.remove('active'));
+}
+
+// buttons must be in the correct order (same as the blocks)
+// for the code below to work correctly
+playerBtn.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        deactivatePrevious();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active');
+    })
+})
